3024cfa8cbce19c0650b789299b5c6411d78ebf1
KevinCiardelli <98409110+KevinCiardelli@users.noreply.github.com>
2022-04-24 11:20:32 -0400

    Update code.py

--- code.py
+++ code.py
@@ -13,6 +13,10 @@ speaker.direction = digitalio.Direction.OUTPUT
 speaker.value = True
 audio = AudioOut(board.SPEAKER)
 
+strip_pin=board.A1
+strip_num_of_lights=56
+strip=neopixel.NeoPixel(strip_pin, strip_num_of_lights, brightness=0.5, auto_write=True)
+
 path = "sounds/"
 
 def play_sound(filename):
@@ -31,18 +35,51 @@ distance_sensor.timing_budget = 100
 distance_sensor.start_ranging()
 
 touchpad_A6 = touchio.TouchIn(board.A6)
-touchpad_A1 = touchio.TouchIn(board.A1)
 touchpad_A3 = touchio.TouchIn(board.A3)
-touchpad_TX = touchio.TouchIn(board.TX)
 
-while True: 
-    if touchpad_A1.value():
+
+def light_up(color):
+    for i in range(strip_num_of_lights):
+        strip[i]= ((color))
+        time.sleep(0.05)
+
+#needs modification based on board-smaller values
+
+def distance_colors(distance):
+    if(distance <= 8 and distance>1):
+        for i in range(56):
+            strip[i]=((0,255,255))
+    elif(distance>8 and distance <= 13):
+        for i in range(45):
+            strip[i]=((0,255,255))
+        for x in range(45, 56):
+            strip[x]= ((0,0,0))
+    elif(distance>13 and distance <= 15):
+        for i in range(30):
+            strip[i]=((0,255,255))
+        for x in range(30, 56):
+            strip[x]= ((0,0,0))
+    elif(distance>15 and distance <= 19):
+        for i in range(15):
+            strip[i]=((0,255,255))
+        for x in range(31, 56):
+            strip[x]= ((0,0,0))
+    elif(distance>19 or distance==0):
+       strip.fill((0,0,0))
+    
+    
+while True:
+    if touchpad_A6.value:
+        light_up((255,0,0))
+        strip.fill((0,0,0))
         #if the touchpad has been pressed
-    if touchpad_A3.value():
+    elif touchpad_A3.value:
+        strip.fill((255,255,0))
+        strip.fill((0,0,0))
         #if the touchpad has been pressed
-    if distance_sensor.data_ready:
+    elif distance_sensor.data_ready:
         #if the slider has been moved
         distance = distance_sensor.distance
-        print(f"Distance: {distance}")
-        time.sleep(0.1)
+        distance_colors(distance)
+        print(distance)
     
