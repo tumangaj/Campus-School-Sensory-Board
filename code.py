b701a8e79a346423719076a501e9800784a284e8
tumangaj <98072886+tumangaj@users.noreply.github.com>
2022-04-26 12:42:03 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,23 +8,10 @@ import digitalio
 from audiopwmio import PWMAudioOut as AudioOut
 from audiocore import WaveFile
 
-speaker = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
-speaker.direction = digitalio.Direction.OUTPUT
-speaker.value = True
-audio = AudioOut(board.SPEAKER)
 
 strip_pin=board.A1
 strip_num_of_lights=56
 strip=neopixel.NeoPixel(strip_pin, strip_num_of_lights, brightness=0.5, auto_write=True)
-
-path = "sounds/"
-
-def play_sound(filename):
-    with open(path + filename, "rb") as wave_file:
-            wave = WaveFile(wave_file)
-            audio.play(wave)
-            while audio.playing:
-                pass
 
 i2c = board.I2C()
 distance_sensor = adafruit_vl53l1x.VL53L1X(i2c)
@@ -37,36 +24,19 @@ distance_sensor.start_ranging()
 touchpad_A6 = touchio.TouchIn(board.A6)
 touchpad_A3 = touchio.TouchIn(board.A3)
 
-
 def light_up(color):
     for i in range(strip_num_of_lights):
         strip[i]= ((color))
         time.sleep(0.05)
-
-#needs modification based on board-smaller values
-
-def distance_colors(distance):
-    if(distance <= 8 and distance>1):
-        for i in range(56):
-            strip[i]=((0,255,255))
-    elif(distance>8 and distance <= 13):
-        for i in range(45):
-            strip[i]=((0,255,255))
-        for x in range(45, 56):
-            strip[x]= ((0,0,0))
-    elif(distance>13 and distance <= 15):
-        for i in range(30):
-            strip[i]=((0,255,255))
-        for x in range(30, 56):
-            strip[x]= ((0,0,0))
-    elif(distance>15 and distance <= 19):
-        for i in range(15):
-            strip[i]=((0,255,255))
-        for x in range(31, 56):
-            strip[x]= ((0,0,0))
-    elif(distance>19 or distance==0):
-       strip.fill((0,0,0))
-    
+        
+def light_up1(color1, color2):
+    for i in range(strip_num_of_lights):
+        if i%2 == 0:
+            strip[i] = ((color1))
+            time.sleep(0.05)
+        else:
+            strip[i]= ((color2))
+            time.sleep(0.05)
     
 while True:
     if touchpad_A6.value:
@@ -80,6 +50,6 @@ while True:
     elif distance_sensor.data_ready:
         #if the slider has been moved
         distance = distance_sensor.distance
-        distance_colors(distance)
+        light_up1((255,255,0), (255,0,0))
         print(distance)
     
